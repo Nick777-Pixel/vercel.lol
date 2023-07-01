023cbf1f67bdbf1996dd71325eeecfdc1fc08a3f
Nick777-Pixel <81410494+Nick777-Pixel@users.noreply.github.com>
2023-07-01 01:07:57 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import "./globals.css";
 const inter = Inter({ subsets: [] });
 
 export async function generateMetadata(): Promise<Metadata> {
-  const description = "▲ The triangle company";
+  const description = "▲ The Triangle Co.";
   const title = "The Triangle Co.";
   return {
     title,
